ecb13f5e3375e77a03e543a08cea8df96d290fa0
shuoy <shuo.yang.2006@gmail.com>
2014-06-23 16:53:46 -0700

    setup testing script for both Ubuntu and CentOS

---
+++ run.sh
@@ -0,0 +1,25 @@
+!#/bin/bash
+
+os_family=$(echo $1 | tr '[:lower:]' '[:upper:]')
+printf "$os_family \n\n"
+
+# Cleanup the Vagrantfile that linked by previous run(s)
+rm ./Vagrantfile
+
+# Make a softlink so that Vagrant can work with the right OS
+if [ $os_family = "DEBIAN" ]; then
+  echo "Run Vagrant machines with Debian images"
+  ln -s ./Vagrantfile.debian ./Vagrantfile
+else
+  echo "Run Vagrant machines with CentOS images"
+  ln -s ./Vagrantfile.redhat ./Vagrantfile
+fi
+
+#Create VMs if needed
+vagrant up
+
+
+# Run the Playbook on to the corresponding VMs
+source rc
+ansible-playbook -i hosts_test site.yml
+
