2542e48ed4c8cb1c4f1fb9ddbaabd11c12a4ff68
RichardCao <Richard@Richard-MacbookPro.local>
2014-07-24 17:31:39 -0700

    half way

--- run.sh
+++ run.sh
@@ -1,25 +1,28 @@
-!#/bin/bash
+#!/bin/bash
 
 os_family=$(echo $1 | tr '[:lower:]' '[:upper:]')
-printf "$os_family \n\n"
+printf "$os_family \n"
+
 
 # Cleanup the Vagrantfile that linked by previous run(s)
-rm ./Vagrantfile
+rm -f ./Vagrantfile
 
-# Make a softlink so that Vagrant can work with the right OS
-if [ $os_family = "DEBIAN" ]; then
+# Make a softlink so that vagrant can work with the right OS
+if [[ $os_family = "DEBIAN" ]]; then
   echo "Run Vagrant machines with Debian images"
   ln -s ./Vagrantfile.debian ./Vagrantfile
-else
+elif [[ $os_family = "REDHAT" ]]; then
   echo "Run Vagrant machines with CentOS images"
   ln -s ./Vagrantfile.redhat ./Vagrantfile
+else
+	echo "[ERROR]Wrong system image!"
+	exit 1
 fi
 
 #Create VMs if needed
 vagrant up
 
 
-# Run the Playbook on to the corresponding VMs
+# Run the playbook on to the corresponding VMs
 source rc
 ansible-playbook -i hosts_test site.yml
-
