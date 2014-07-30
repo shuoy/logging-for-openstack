a38effb0108da7a603d3bc29df517d127719df9d
RichardCao <create0818@163.com>
2014-07-30 16:09:32 -0700

    deploy on centos-mini-1-4 with-host-problem

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Run the playbook on to the corresponding VMs
-#source rc
-ansible-playbook -i hosts_test site.yml
+export ANSIBLE_CONFIG=.ansible.cfg
+ansible-playbook -i hosts site.yml
